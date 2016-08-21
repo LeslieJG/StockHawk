b11804b32bab2b54e5febfedd5ac5675b98f4c84
LeslieJG <lgelberger@gmail.com>
2016-08-22 04:13:25 +1200

    Add detail fragmnet/activity whichs is iopened when stock is clicked

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailActivity.java
@@ -17,5 +17,29 @@ public class DetailActivity extends AppCompatActivity {
 
         Log.v(LOG_TAG, "LJG Detail Activity - onCreate");
 
+        if (savedInstanceState == null) {
+            // Create the detail fragment and add it to the activity
+            // using a fragment transaction.
+
+            Bundle arguments = new Bundle();
+           // arguments.putParcelable(DetailFragment.DETAIL_URI, getIntent().getData());
+          //  arguments.putBoolean(DetailFragment.DETAIL_TRANSITION_ANIMATION, true);
+
+            DetailFragment fragment = new DetailFragment();
+          //  fragment.setArguments(arguments);
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.detail_fragment_container, fragment)
+                    .commit();
+
+            // Being here means we are in animation mode
+            supportPostponeEnterTransition();
+        }
+
+
+
+
+
+
     }
 }
