dc49d1b27d0fdbb78d6cdbadbf0fcd14b31f3fad
LeslieJG <lgelberger@gmail.com>
2016-09-13 06:51:34 +1200

    Set up blank widget configure activity

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockQuoteWidgetConfigure.java
@@ -0,0 +1,21 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Leslie on 2016-09-12.
+ *
+ * For Handling the One-time configuration of each widget.
+ *
+ * This allows the users to pick which stock the widget displays,
+ * allowing for many widgets each displaying their own stock information.
+ *
+ * Modelled after https://android.googlesource.com/platform/development/+/master/samples/ApiDemos/src/com/example/android/apis/appwidget/ExampleAppWidgetConfigure.java
+ */
+public class StockQuoteWidgetConfigure extends AppCompatActivity {
+
+    
+
+
+
+}
